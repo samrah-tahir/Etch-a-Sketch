52d1e1bb2e3ee0cb3cc6496f0d9e55d14d0d5cfc
samrah-tahir <samrah.tahir11@gmail.com>
2022-06-19 17:29:36 +0400

    Added a button to enter size of grid

--- sketch.js
+++ sketch.js
@@ -1,33 +1,54 @@
+
 function createGrid(numOfSq){
+
     let numOfBoxes = numOfSq;
     const gridLen = 500; //500 x 500
 
     let widthPerBox = gridLen/numOfBoxes;
     let heightPerBox = widthPerBox;
-    
-    // let gridBox = document.querySelector(".grid");
+
     let gridBox = document.querySelector(".grid");
     gridBox.style.display = "flex";
     gridBox.style.flexWrap = "wrap";
-
+    gridBox.innerHTML = '';
+    clearGrid(gridBox);
     for(let i = 0; i < numOfBoxes * numOfSq; i++){
         let box = document.createElement("div");
+
         box.classList.add("grid-element");
         box.style.width = widthPerBox.toString()+"px";
         box.style.height = heightPerBox.toString()+"px";
-        
         gridBox.appendChild(box);
     }
-    // hoverEffect();
+    hoverEffect();
+}
+
+function clearGrid(gridBox){
+    gridBox.innerHTML = '';
 }
 
 function hoverEffect(){
     let grid_Elements = document.querySelectorAll(".grid-element");
 
     grid_Elements.forEach( (element) => {
-        element.addEventListener()
+        element.addEventListener("mouseover", changeColor);
     });
-    console.log(grid_Elements);
+    // console.log(grid_Elements);
 
 }
-createGrid(6);
+
+function changeColor(e){
+    if(e.type === "mouseover") 
+        e.target.style.backgroundColor = "black";
+}
+
+function getSize(){
+    let size = document.querySelector('input[name="size"]');
+    let submit = document.querySelector('input[type="button"]');
+
+    submit.addEventListener('click', () => createGrid(Number(size.value)));
+}
+
+let clearButton = document.querySelector(".btn-clear");
+clearButton.addEventListener('click', () => createGrid(6));
+getSize();
